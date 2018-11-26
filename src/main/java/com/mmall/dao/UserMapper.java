4992b1854807c708bbb0fbfd7babd77656f6145e
刘斐笑 <liufeixiao@liufeixiaodeiMac.local>
2018-11-26 17:33:23 +0800

    user 11.26

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.mmall.dao;
 
+import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.pojo.UserExample;
 import org.apache.ibatis.annotations.Param;
@@ -63,5 +64,17 @@ public interface UserMapper {
 
     int checkUsername(String username);
 
+    int checkEmail(String email);
+
     User selectLogin(@Param("username") String username, @Param("password") String password);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(@Param("username")String username,@Param("question") String question,@Param("answer") String answer);
+
+    int updatePasswordByUsername(@Param("username") String username,@Param("passwordNew") String passwordNew);
+
+    int checkPassword(@Param(value = "password") String password,@Param("userId") Integer userId);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -5,4 +5,16 @@ import com.mmall.pojo.User;
 
 public interface IUserService {
     ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str,String type);
+
+    ServerResponse selectQuestion(String useranme);
+
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+    ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld,String passwordNew, User user);
 }
